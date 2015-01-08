4f8e0e8a213fba6e61fc3e392eba3100a132e909
fll <flopezluro@gmail.com>
2015-01-08 09:24:42 +0100

    removed some comments and unused imports

--- Elements/EmptyState/EmptyState.py
+++ Elements/EmptyState/EmptyState.py
@@ -2,32 +2,28 @@ __author__ = 'Francholi'
 __author__ = 'Francholi'
 
 # panda imports
-from direct.gui.DirectGui import *
-from direct.task.Task import *
-from panda3d.core import *
 from Element import *
-
-#sys utils
-import sys
 
 class EmptyState(Element):
     """
     As the name indicates, it's an empty state. It does nothing.
-    """
+    It can react to timeout events using:
+    timeout: float
 
+    """
     def __init__(self, **kwargs):
         """
-        world is a reference to the main class
+        See the Element class to find out what attributes are available
+        from scratch
         """
         super(EmptyState, self).__init__(**kwargs)
         self.hideElement()
 
     def enterState(self):
-        # print "entering ScreenText"
         # super class enterState
         Element.enterState(self)
 
     def exitState(self):
-        # print "leaving state ScreenText"
+        # super class exitState
         Element.exitState(self)
 
