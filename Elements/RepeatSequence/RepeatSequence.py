c1e5e0e58be8c0355746a67d8c195166fa179803
francholi <flopezluro@gmail.com>
2016-05-29 20:06:33 +0200

    added RepeatSequence node, that can loop through a series of values for a global variable and send a message each time to either repeat or to end the repetition.

---
+++ Elements/RepeatSequence/RepeatSequence.py
@@ -0,0 +1,47 @@
+__author__ = 'Francholi'
+__author__ = 'Francholi'
+
+# panda imports
+from Element import *
+import random
+
+class RepeatSequence(Element):
+    """
+    Set up a global variable name, set one value to it when entering
+    to the Element, then set a new value every time until all values
+    have been set.
+    This Element has to remember if it has been called already before.
+    """
+    def __init__(self, **kwargs):
+        """
+        See the Element class to find out what attributes are available
+        from scratch
+        """
+        super(RepeatSequence, self).__init__(**kwargs)
+        self.hideElement()
+
+    def enterState(self):
+        # super class enterState
+        Element.enterState(self)
+        varName = self.config.variable
+        # FIRST TIME, THE KEY DOES NOT EXIST.
+        if varName not in self.config.world.globals.keys():
+            # take the list of values, save it, randomize if necessary.
+            self.values = self.config.values
+            if self.config.random:
+                random.shuffle(self.values)
+        # ALWAYS DO THIS
+        if len(self.values):
+            self.config.world.globals[varName] = self.values.pop()
+            self.sendMessage('repeat')
+        else:
+            self.sendMessage('end')
+
+    def exitState(self):
+        # super class exitState
+        Element.exitState(self)
+
+    def getConfigTemplate(self):
+        elementDict = Element.getConfigTemplate()
+        return elementDict
+        
